8fd65581954548497e6512e21ab7e8883c9103e7
Francisco Ramos <fro.profesional@gmail.com>
2023-04-06 23:08:26 -0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export {
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export { getQuery } from "https://deno.land/x/oak@v12.1.0/helpers.ts";
 export { Schema, z, ZodError } from "https://deno.land/x/zod@v3.21.4/mod.ts";
-export { DBClient } from "https://raw.githubusercontent.com/clau-org/api-db/v0.0.4/src/db.ts";
+export { DBClient } from "https://cdn.jsdelivr.net/gh/clau-org/api-db@latest/src/db.ts";
